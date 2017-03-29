53de4e937be774d608716b14b4bc18b3fffe1abb
mac <mac@nevada.unr.edu>
2017-03-28 17:47:31 -0700

    first pass at 2D height map generation. there is a clipping issue that needs to be investigated

---
+++ src/lidar/catkin_generated/setup_cached.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/mac/Developer/ros-examples/src/lidar/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH="/opt/ros/indigo/share/euslisp/jskeus/eus//Linux64/lib:/opt/ros/indigo/lib:/opt/ros/indigo/share/euslisp/jskeus/eus//Linux64/lib:/usr/local/cuda-7.5/lib64"
+export PATH="/opt/ros/indigo/share/euslisp/jskeus/eus//Linux64/bin:/opt/ros/indigo/bin:/opt/ros/indigo/share/euslisp/jskeus/eus//Linux64/bin:/usr/local/cuda-7.5/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/mac/Developer/ros-examples/src/lidar/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/mac/Developer/ros-examples/src/lidar:$ROS_PACKAGE_PATH"
--- src/lidar/src/lidar_node.cpp
+++ src/lidar/src/lidar_node.cpp
@@ -4,24 +4,14 @@
 #include <vector>
 #include <float.h>
 #include <stdio.h>
+#include <math.h> 
 
 #include <pcl_conversions/pcl_conversions.h>
 #include <pcl/point_cloud.h>
 #include <pcl/point_types.h>
 #include <pcl/filters/voxel_grid.h>
 #include <pcl/filters/extract_indices.h>
-#include <pcl/kdtree/kdtree_flann.h>
-#include <pcl/segmentation/progressive_morphological_filter.h>
-#include <pcl/ModelCoefficients.h>
 #include <pcl/point_types.h>
-#include <pcl/filters/voxel_grid.h>
-#include <pcl/features/normal_3d.h>
-#include <pcl/kdtree/kdtree.h>
-#include <pcl/sample_consensus/method_types.h>
-#include <pcl/sample_consensus/model_types.h>
-#include <pcl/segmentation/sac_segmentation.h>
-#include <pcl/segmentation/extract_clusters.h>
-#include <pcl/common/centroid.h>
 #include <pcl/io/pcd_io.h>
 
 #include <ros/ros.h>
@@ -37,41 +27,95 @@ ros::Subscriber subPointCloud;
 ros::Publisher pubPointCloud;
 
 pcl::PointCloud<pcl::PointXYZ>::Ptr cloud (new pcl::PointCloud<pcl::PointXYZ>);
-pcl::PassThrough<pcl::PointXYZ> pass;
-pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_filtered (new pcl::PointCloud<pcl::PointXYZ>);
+pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_grid (new pcl::PointCloud<pcl::PointXYZ>);
+sensor_msgs::PointCloud2 output;
 
-double heightArray[IMAGE_HEIGHT][IMAGE_WIDTH];	// for a 700x800 image around the central lidar point [250][400]
+double heightArray[IMAGE_HEIGHT][IMAGE_WIDTH];
 
 // map meters to index
-void map(double x, double y, int &row, int &column){
+// returns 0 if not in range, 1 if in range and row/column are set
+int map_pc2rc(double x, double y, int *row, int *column){
+  // Check if falls within range
+  if(x > (IMAGE_WIDTH * -0.5 * 0.1) && x < (IMAGE_WIDTH * 0.5 * 0.1) && y > (IMAGE_HEIGHT * -0.5 * 0.1) && y < (IMAGE_HEIGHT * 0.5 * 0.1)){
+    // Find x -> row mapping
+    *row = (int)round(floor(((((IMAGE_HEIGHT*BIN)/2.0) - x)/(IMAGE_HEIGHT*BIN)) * IMAGE_HEIGHT));	// obviously can be simplified, but leaving for debug
 
+    // Find y -> column mapping
+    *column = (int)round(floor(((((IMAGE_WIDTH*BIN)/2.0) - y)/(IMAGE_WIDTH*BIN)) * IMAGE_WIDTH));
+
+    // Return success
+    return 1;
+    }
+
+  return 0;
   }
 
+// map index to meters
+// returns 0 if not in range, 1 if in range and x/y are set
+int map_rc2pc(double *x, double *y, int row, int column){
+  // Check if falls within range
+  if(row >= 0 && row < IMAGE_HEIGHT && column >= 0 && column < IMAGE_WIDTH){
+    // Find row -> x mapping
+    *x = (double)(BIN*-1.0 * (row - (IMAGE_HEIGHT/2.0)));	// this one is simplified
+
+    // column -> y mapping
+    *y = (double)(BIN*-1.0 * (column - (IMAGE_WIDTH/2.0)));
+
+    // Return success
+    return 1;
+    }
+
+  return 0;
+  }
+
+// main generation function
 void DEM(const sensor_msgs::PointCloud2ConstPtr& pointCloudMsg)
 {
   ROS_DEBUG("Point Cloud Received");
 
-  // Clip
-  pass.setInputCloud(cloud);
-  pass.setFilterFieldName ("z");
-
-  // Zero out height map
-  for(int i = 0; i < (IMAGE_HEIGHT*IMAGE_WIDTH); ++i){
-    // 
-    heightArray[i] = (-FLT_MAX);
+  // clear cloud and height map array
+  for(int i = 0; i < IMAGE_HEIGHT; ++i){ 
+    for(int j = 0; j < IMAGE_WIDTH; ++j){ 
+      heightArray[i][j] = (double)(-FLT_MAX);
+      }
     }
 
   // Convert from ROS message to PCL point cloud
   pcl::fromROSMsg(*pointCloudMsg, *cloud);
+//ROS_ERROR("%d", (int)cloud->points.size());
 
-  // Clip point cloud to the range specified by the paper
+  // Populate the DEM grid by looping through every point
+  int row, column;
+  for(size_t j = 0; j < cloud->points.size(); ++j){
+//ROS_ERROR("%f,%f,%f", cloud->points[j].x,cloud->points[j].y,cloud->points[j].z);
+    // If the point is within the image size bounds
+    if(map_pc2rc(cloud->points[j].x, cloud->points[j].y, &row, &column) == 1){
+      if(cloud->points[j].z > heightArray[row][column]){
+//ROS_ERROR("%d,%d", row,column);
+        heightArray[row][column] = cloud->points[j].z;
 
-  // Populate the DEM by looping through every point
-  for (size_t j = 0; j < cloud->points.size (); ++j){
-    // Check bin
+        }
+      }
     }
-}
 
+  // Create "point cloud" to be published for visualization and later python node PNG generation
+  int index = 0;
+  for(int i = 0; i < IMAGE_HEIGHT; ++i){ 
+    for(int j = 0; j < IMAGE_WIDTH; ++j){ 
+      // Add point
+      index = i * j;
+      //cloud_grid->points[index].x = (double)j;
+      //cloud_grid->points[index].y = (double)i;
+      cloud_grid->points[index].z = heightArray[i][j];
+      }
+    }
+
+  // Output height map to point cloud for python node to parse to PNG
+  pcl::toROSMsg(*cloud_grid, output);
+  output.header.stamp = ros::Time::now();
+  output.header.frame_id = "velodyne";
+  pubPointCloud.publish(output);
+}
 
 int main(int argc, char** argv)
 {
@@ -79,7 +123,28 @@ int main(int argc, char** argv)
   ros::init(argc, argv, "lidar_node");
   ros::NodeHandle nh;
 
-  subPointCloud = nh.subscribe<sensor_msgs::PointCloud2>("/velodyne_points", 2, DEM);
+  cloud_grid->width  = IMAGE_WIDTH;
+  cloud_grid->height = IMAGE_HEIGHT;
+  cloud_grid->points.resize (cloud_grid->width * cloud_grid->height);
+ 
+  // Setup indicies in point clouds
+  int index = 0;
+  double x, y;
+  for(int i = 0; i < IMAGE_HEIGHT; ++i){
+    for(int j = 0; j < IMAGE_WIDTH; ++j){
+      index = i * j;
+      (void)map_rc2pc(&x, &y, i, j);
+//ROS_ERROR("%f,%f", x,y);
+      cloud_grid->points[index].x = x;
+      cloud_grid->points[index].y = y;
+      cloud_grid->points[index].z = (-FLT_MAX);
+      // Temp storage
+      heightArray[i][j] = (-FLT_MAX);
+      }
+    }
+
+  subPointCloud = nh.subscribe<sensor_msgs::PointCloud2>("/points_raw", 2, DEM);
+  pubPointCloud = nh.advertise<sensor_msgs::PointCloud2> ("/heightmap/pointcloud", 1);
 
   ros::spin();
 
